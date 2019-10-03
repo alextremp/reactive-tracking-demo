84dc8d97fdaa45c01c1f4e3df9ce87f7e15bcd6a
Alex Castells <alextremp@hotmail.com>
2019-10-03 18:20:12 +0200

    poc

--- src/main/java/com/github/alextremp/reactivetrackingdemo/infrastructure/framework/ReactiveTrackingDemoApplication.java
+++ src/main/java/com/github/alextremp/reactivetrackingdemo/infrastructure/framework/ReactiveTrackingDemoApplication.java
@@ -1,9 +1,11 @@
-package com.github.alextremp.reactivetrackingdemo;
+package com.github.alextremp.reactivetrackingdemo.infrastructure.framework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class ReactiveTrackingDemoApplication {
 
   public static void main(String[] args) {
