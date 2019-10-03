34d8fa6edeca82038e2f95df330700120884a510
Alex Castells <alextremp@hotmail.com>
2019-10-03 17:40:49 +0200

    Add Spring Boot Initializr generated content

---
+++ src/test/java/com/github/alextremp/reactivetrackingdemo/ReactiveTrackingDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.alextremp.reactivetrackingdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReactiveTrackingDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
