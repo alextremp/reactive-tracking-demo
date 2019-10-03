9a73efbb31537a3034ec89921584b179bf1a5fd3
Alex Castells <alextremp@hotmail.com>
2019-10-03 17:44:49 +0200

    add codestyle

--- src/main/java/com/github/alextremp/reactivetrackingdemo/ReactiveTrackingDemoApplication.java
+++ src/main/java/com/github/alextremp/reactivetrackingdemo/ReactiveTrackingDemoApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ReactiveTrackingDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ReactiveTrackingDemoApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(ReactiveTrackingDemoApplication.class, args);
+  }
 
 }
